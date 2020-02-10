bf632ed486971db3e35387524e05f7a3477c80e0
CaptainVegeta <murat.tokmakci@outlook.com>
2020-02-10 10:36:24 +0100

    added 04

---
+++ 04_Klassen/Aufgaben.java
@@ -0,0 +1,10 @@
+public class Aufgaben{
+    public static void main(String[] args) {
+        ZweiteKlasse.zahl1 = 10.55f;
+        ZweiteKlasse.zahl2 =  9.45f;
+
+        ZweiteKlasse.floatRechner();
+
+        public static int test = 1;
+    }
+}
---
+++ 04_Klassen/ZweiteKlasse.java
@@ -0,0 +1,12 @@
+public class ZweiteKlasse{
+    public static float zahl1 = 5.55f;
+    public static float zahl2 = 4.45f;
+
+    public static void floatRechner(){
+        float summe = zahl1 + zahl2;
+        System.out.println(summe);
+
+        System.out.println(zahl1);
+        System.out.println(zahl2);
+    }
+}
