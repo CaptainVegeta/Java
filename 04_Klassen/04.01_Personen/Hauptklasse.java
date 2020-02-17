25a3e001a30f45d62c09869be4bcaf373247b3d3
CaptainVegeta <murat.tokmakci@outlook.com>
2020-02-17 09:40:45 +0100

    generated persons with an array

--- 04_Klassen/04.01_Personen/Hauptklasse.java
+++ 04_Klassen/04.01_Personen/Hauptklasse.java
@@ -4,25 +4,34 @@
 public class Hauptklasse {
 
     public static void main(String[] args) {
-        Person person1;
-        person1 = new Person();
-        person1.aendereAttribute("Murat", 26, 1.75f);
 
-        Person person2;
-        person2 = new Person();
-        person2.aendereAttribute("Dan", 40, 1.80f);
+        int arrayGroesse = 10;
+        Person[] person = new Person[arrayGroesse];
 
-        Person person3;
-        person3 = new Person();
-        person2.aendereAttribute("Miguel", 29, 1.80f);
+        for (int i = 0; i < person.length; i++) {
+            person[i] = new Person();
+        }
 
-        Person person4;
-        person4 = new Person();
-        person2.aendereAttribute("Lukas", 26, 1.78f);
+        person[0].aendereAttribute("Murat", 26, 1.75f);
+
+        person[1].aendereAttribute("Murat", 26, 1.75f);
+
+        person[2].aendereAttribute("Dan", 40, 1.80f);
+
+        person[3].aendereAttribute("Miguel", 29, 1.80f);
+
+        person[4].aendereAttribute("Lukas", 26, 1.78f);
         
-        Person person5;
-        person5 = new Person();
-        person2.aendereAttribute("Son Goku", 100, 2.0f);
+        person[5].aendereAttribute("Son Goku", 100, 2.0f);
 
+        person[6].aendereAttribute("Murat", 26, 1.75f);
+
+        person[7].aendereAttribute("Murat", 26, 1.75f);
+
+        person[8].aendereAttribute("Dan", 40, 1.80f);
+
+        person[9].aendereAttribute("Miguel", 29, 1.80f);        
     }
+
+    
 }
