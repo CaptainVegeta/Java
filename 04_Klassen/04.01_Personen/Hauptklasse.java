4fe7ba02b6765e51243d2b612de07911da3e27c1
CaptainVegeta <murat.tokmakci@outlook.com>
2020-02-17 10:42:55 +0100

    small changes

--- 04_Klassen/04.01_Personen/Hauptklasse.java
+++ 04_Klassen/04.01_Personen/Hauptklasse.java
@@ -7,29 +7,18 @@ public class Hauptklasse {
 
         int arrayGroesse = 10;
         Person[] person = new Person[arrayGroesse];
-
         for (int i = 0; i < person.length; i++) {
             person[i] = new Person();
         }
-
         person[0].aendereAttribute("Murat", 26, 1.75f);
-
         person[1].aendereAttribute("Michael", 26, 1.75f);
-
         person[2].aendereAttribute("Dan", 40, 1.80f);
-
         person[3].aendereAttribute("Miguel", 29, 1.80f);
-
         person[4].aendereAttribute("Lukas", 26, 1.78f);
-        
         person[5].aendereAttribute("Son Goku", 100, 2.0f);
-
         person[6].aendereAttribute("Martin", 26, 1.75f);
-
         person[7].aendereAttribute("Alex", 28, 1.75f);
-
         person[8].aendereAttribute("Daniel", 42, 1.80f);
-
         person[9].aendereAttribute("Runaldu", 32, 1.80f);
 
         // Alter berechnen
@@ -43,7 +32,6 @@ public class Hauptklasse {
         System.out.println("Das Durchschnittsalter beträg: "+durschnittsAlter);
 
         // Ausgabe wer unter/über dem Durchschnitt ist
-
         for (int i = 0; i < person.length; i++) {
             if (person[i].alter < durschnittsAlter) {
                 System.out.println(person[i].name+" ist unter dem Durschnittsalter.");
@@ -51,7 +39,6 @@ public class Hauptklasse {
             else{
                 System.out.println(person[i].name+" ist über dem Durschnittsalter.");
             }
-            
         }
     }
 }
